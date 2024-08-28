def6c1cf294ef0b79508fd2d6fbe89d5e176067f
James Turnbull <james@lovedthanlost.net>
2024-08-28 18:25:19 -0400

    Bump version to v1.2.4

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ obj.__index = obj
 
 -- Metadata
 obj.name = "Weather"
-obj.version = "1.2.3"
+obj.version = "1.2.4"
 obj.author = "James Turnbull <james@lovedthanlost.net>"
 obj.license = "MIT"
 obj.homepage = "https://github.com/jamtur01/Weather.spoon"
